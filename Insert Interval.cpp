6ad1fc7cd185c8fc9f08d5b5936d52d9fbfbf27b
Zhou Xin <zxthirteen@gmail.com>
2013-02-19 22:37:56 +0800

    插入区间，先考虑两头插入情况，再考虑中间要部分进行改变这种情况。

--- Insert Interval.cpp
+++ Insert Interval.cpp
@@ -7,33 +7,45 @@
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
+bool compInterval(Interval a, Interval b) {
+        return a.start < b.start;
+}
+
 class Solution {
 public:
     vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if (intervals.empty()) return intervals;
+        if (intervals.empty()) {
+            intervals.push_back(newInterval);
+            return intervals;
+        }
         sort(intervals.begin(), intervals.end(), compInterval);
         
+        int n = intervals.size();
         if (newInterval.end < intervals[0].start) {
             intervals.insert(intervals.begin(), newInterval);
         } else if (newInterval.start > intervals[n-1].end){
             intervals.insert(intervals.end(), newInterval);
         } else {
             int startpos = 0, endpos = n-1;
-            while (interval[startpos+1].start <= newInterval.end) startpos++;
-            while (interval[endpos-1].end <= newInterval.start) endpos--;
+            while (intervals[startpos].end < newInterval.start) 
+                startpos++;
+            while (intervals[endpos].start > newInterval.end) 
+                endpos--;
             
-            intervals[endpos].start = min(intarval[startpos].start, newInterval.start);
-            intervals[endpos].end = max(intarval[endpos].end, newInterval.end);
-            
-            intervals.erase(intervals.begin() + startpos, intervals.begin() + endpos);
+            if (startpos <= endpos) {
+                intervals[endpos].start = min(intervals[startpos].start, newInterval.start);
+                intervals[endpos].end = max(intervals[endpos].end, newInterval.end);
+                
+                intervals.erase(intervals.begin() + startpos, intervals.begin() + endpos);
+                // no change to the original intervals, just insert the new one
+            } else {
+                intervals.insert(intervals.begin() + startpos, newInterval);
+            }
+        
         }
         
         return intervals;
-    }
-    
-    bool compInterval(Interval a, Interval b) {
-        return a.start < b.start;
     }
 };
