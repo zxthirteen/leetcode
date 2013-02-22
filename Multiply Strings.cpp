41a2fee6a4986ca4cea7ffff6d769f7d8f4d7667
Zhou Xin <zxthirteen@gmail.com>
2013-02-22 17:19:03 +0800

    大字符串相乘

---
+++ Multiply Strings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int m = num1.size(), n = num2.size();
+        int *x = new int[m+n];
+        
+        memset(x, 0, sizeof(int)*(m+n));
+        
+        for (int i = m-1; i >= 0; i--) {
+            for (int j = n-1; j >= 0; j--) {
+                x[i+j+1] += (num1[i]-'0') * (num2[j]-'0');
+            }
+        }
+        
+        for (int i = m+n-1; i > 0; i--) {
+            x[i-1] += x[i]/10;
+            x[i] = x[i]%10;
+        }
+        
+        int start = 0;
+        while (x[start] == 0 && start < m+n-1) start++;
+        
+        string res(m+n-start, '0');
+        for (int i = 0; i < m+n-start; i++) {
+            res[i] = x[start+i] + '0';
+        }
+        
+        return res;
+    }
+};
