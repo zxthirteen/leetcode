1b3eafb1cf31b652a1d4bb352eea8bbaac6a609a
Zhou Xin <zxthirteen@gmail.com>
2013-03-27 23:49:33 +0800

    Update Search a 2D Matrix.cpp

--- Search a 2D Matrix.cpp
+++ Search a 2D Matrix.cpp
@@ -3,19 +3,20 @@ public:
     bool searchMatrix(vector<vector<int> > &matrix, int target) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if (matrix.empty() || matrix[0].empty()) return false;
-        int m = matrix.size(), n = matrix[0].size();
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int l = 0;
+        int r = n*m-1;
         
-        int x = m -1, y = 0;
-        while (true) {
-            if (matrix[x][y] == target) return true;
-            if (matrix[x][y] > target) {
-                if (x == 0) return false;
-                else x = x -1;
-            } else {
-                if (y == n -1) return false;
-                else y = y +1;
-            }
+        while (l <= r) {
+            int mid = (l+r)/2;
+            int i = mid/m;
+            int j = mid%m;
+            if (matrix[i][j] == target) return true;
+            if (matrix[i][j] > target)
+                r = mid-1;
+            else 
+                l = mid+1;
         }
         
         return false;
